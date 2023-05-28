353e14024ba84b5c186b9b241e5cdcc5d470facf
michelleroydelarredeladorie <132815508+michelleroydelarredeladorie@users.noreply.github.com>
2023-05-28 11:37:44 +0200

    Correction TestBicycleFactory.java

--- src/test/TestBicycleFactory.java
+++ src/test/TestBicycleFactory.java
@@ -6,54 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import core.Bicycle;
 import core.BicycleFactory;
-import core.ElectricalBicycle;
-import core.MechanicalBicycle;
-
-class TestBicycleFactory {
-
-	@Test
-	void ifBicycleRequiredIsElectricalTheReturnedIsElectrical() {
-		BicycleFactory Factory = new BicycleFactory();
-		Bicycle bike = Factory.getBicycle("Electrical");
-		if (bike instanceof ElectricalBicycle) {
-			assert(true);
-		}
-		else {
-			assert(false);
-		}
-	}
-	
-	@Test
-	void ifBicycleRequiredIsMechanicalTheReturnedIsMechanical() {
-		BicycleFactory Factory = new BicycleFactory();
-		Bicycle bike = Factory.getBicycle("Mechanical");
-		if (bike instanceof MechanicalBicycle) {
-			assert(true);
-		}
-		else {
-			assert(false);
-		}
-	}
-	@Test
-	void ifBicycleRequiredNotMecaOrElectricalReturnNull() {
-		BicycleFactory Factory = new BicycleFactory();
-		Bicycle bike = Factory.getBicycle("whatever");
-		
-		if (bike == null) {
-			assert(true);
-		}
-		else {
-			assert(false);
-		}
-	}
-	package test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-import core.Bicycle;
-import core.BicycleFactory;
 import core.Cards;
 import core.CardsFactory;
 import core.ElectricalBicycle;
@@ -118,7 +70,5 @@ class TestBicycleFactory {
 	    assertEquals(0, result);
 	}
 
-
-}
 
 }
