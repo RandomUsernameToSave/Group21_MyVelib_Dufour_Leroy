f5732dcb275b94cec534ffc5116baaefcb393d04
michelleroydelarredeladorie <132815508+michelleroydelarredeladorie@users.noreply.github.com>
2023-05-29 23:44:16 +0200

    Update TestUser.java

--- src/test/TestUser.java
+++ src/test/TestUser.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.UUID;
 import core.GPS;
+import core.MechanicalBicycle;
+
 import org.junit.jupiter.api.Test;
 
 import core.Bicycle;
@@ -12,7 +14,9 @@ import core.BicycleFactory;
 import core.Cards;
 import core.CardsFactory;
 import core.DockingStation;
+import core.ElectricalBicycle;
 import core.Parking;
+import core.StandardStation;
 import core.User;
 import core.plusStation;
 
@@ -45,5 +49,78 @@ class TestUser {
 		}
 
 	}
+	
+	@Test
+	public void testnbRides() {
+		CardsFactory Factory = new CardsFactory();
+		Cards card = Factory.getCards("Vmax");
+		
+		MechanicalBicycle bike = new MechanicalBicycle();
+		GPS gps = new GPS(48.8566, 2.3522);
+        StandardStation station = new StandardStation(5, gps);
+	    User Thomas = new User("Thomas", UUID.randomUUID(), card);
+	    Thomas.setCurrentBicycle(bike);
+	    bike.setUser(Thomas);
+		Thomas.setTimecreditBalance(25);
+		Thomas.setTotalCharge(1);
+		Thomas.returnBike(station,140);
+		
+	    //Assert Total Charge correctly changed
+	    assertEquals(1, Thomas.getnbRides());
+	}
 
+	@Test
+	public void testReturnMechanicalBike() {
+		CardsFactory Factory = new CardsFactory();
+		Cards card = Factory.getCards("Vmax");
+		
+		MechanicalBicycle bike = new MechanicalBicycle();
+		GPS gps = new GPS(48.8566, 2.3522);
+        StandardStation station = new StandardStation(5, gps);
+	    User Thomas = new User("Thomas", UUID.randomUUID(), card);
+	    Thomas.setCurrentBicycle(bike);
+	    bike.setUser(Thomas);
+		Thomas.setTimecreditBalance(25);
+		Thomas.setTotalCharge(1);
+		Thomas.returnBike(station,140);
+		
+	    //Assert has Mechanical
+	    assertTrue(station.hasBikeType("Mechanical"));
+	}
+	@Test
+	public void testReturnElectricalBike() {
+		CardsFactory Factory = new CardsFactory();
+		Cards card = Factory.getCards("Vmax");
+		ElectricalBicycle bike = new ElectricalBicycle();
+		GPS gps = new GPS(48.8566, 2.3522);
+        StandardStation station = new StandardStation(5, gps);
+	    User Thomas = new User("Thomas", UUID.randomUUID(), card);
+	    Thomas.setCurrentBicycle(bike);
+	    bike.setUser(Thomas);
+		Thomas.setTimecreditBalance(25);
+		Thomas.setTotalCharge(1);
+		Thomas.returnBike(station,140);
+		
+	    //Assert has Mechanical
+	    assertTrue(station.hasBikeType("Electrical"));
+	    //Assert the bike has been dropped
+	    assertEquals(null, Thomas.getCurrentBicycle());
+	}
+	@Test
+	public void testReturnWildBike() {
+		CardsFactory Factory = new CardsFactory();
+		Cards card = Factory.getCards("Vmax");
+		ElectricalBicycle bike = new ElectricalBicycle();
+		GPS gps = new GPS(48.8566, 2.3522);
+	    User Thomas = new User("Thomas", UUID.randomUUID(), card);
+	    Thomas.setCurrentBicycle(bike);
+	    bike.setUser(Thomas);
+		Thomas.setTimecreditBalance(0);
+		Thomas.setTotalCharge(1);
+		Thomas.returnBike(gps,140);
+		
+
+	    //Price penalty
+	    assertEquals(2.2, Thomas.gettotalCharge());
+	}
 }
